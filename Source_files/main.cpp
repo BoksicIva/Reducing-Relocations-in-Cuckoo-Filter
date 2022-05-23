812368a75f274db744c9de08bf9efd16a5900915
kmirkovic <kmirkovic@adscanner.tv>
2022-05-23 19:04:15 +0200

    Argumenti se primaju preko komandne linije

--- Source_files/main.cpp
+++ Source_files/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <cstdlib>
 #include "../Header_files/implementation.h"
-#include <typeinfo>
-#include <openssl/sha.h>
-#include "../Header_files/hashing.h"
 
 using namespace std;
 
-int main() {
-	int k = 100;
+int main(int argc, char* argv[]) {
+	/*int k = 100;
 	int rows = 50000;
 	int columns = 100;
 	int mnk = 100;
 	int num_of_random_k_mers =5000000;
 	bool reduced = true;
-	string filename = "Reducing-Relocations-in-Cuckoo-Filter/Resource_files/genome.fna";
+	string filename = "Reducing-Relocations-in-Cuckoo-Filter/Resource_files/genome.fna";*/
+
+	if (argc < 8) {
+		cout << "Arguments missing!" << endl;
+		return -1;
+	}
+
+	int k = atoi(argv[1]);
+	int rows = atoi(argv[2]);
+	int columns = atoi(argv[3]);
+	int mnk = atoi(argv[4]);
+	int num_of_random_k_mers = atoi(argv[5]);
+	bool reduced = bool(argv[6]);
+	string filename = argv[7];
 
 	implementation(filename, k, rows, columns, mnk, num_of_random_k_mers, reduced);
 
